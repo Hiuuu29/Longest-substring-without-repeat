f1f8eabc6a0ab6ac92f9e1007bdb2804263406b7
Hieu <hieuuu29@gmail.com>
2025-01-07 21:58:34 +0700

    updated comment

--- Longest_substring_without_repeat.cpp
+++ Longest_substring_without_repeat.cpp
@@ -6,11 +6,14 @@ using namespace std;
 
 int main(){
 
+    // Phương pháp 2 con trỏ
+    // tạo các biến cần thiết, con trỏ bên trái, biến lưu độ dài chuỗi
     string s = "pwwkew";
     unordered_map<char,int> mp;
     int left = 0;
     int result = 0;
 
+    // duyệt qua mảng bằng con trỏ phải
     for (int right = 0; right < s.length(); right++)
     {
         // ktra xem trong map co xuat hien ki tu nay chua
@@ -25,7 +28,8 @@ int main(){
         }
         // khi da xoa xong ki tu trung dau tien, them ki tu trung hien tai vao map
         mp[s[right]]++;
-        result = max(result, right - left + 1); // cap nhat do lon cua chuoi khi xuat hien trung ki tu
+        // cap nhat do lon cua chuoi 
+        result = max(result, right - left + 1); //so sánh chuỗi với chuỗi sau khi add thêm 1 kí tự mới
         
     }
     cout << result << endl;
